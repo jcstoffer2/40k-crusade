73ad38956661c984b363c8b602f28dcb47bbd6b2
Jordan <jcstoffer2@gmail.com>
2020-09-13 18:57:37 -0700

    Make all props but name nullable on Forces and Units. Add default values.

--- app/src/main/java/com/bestdamn/fortyk/crusade/domain/Force.kt
+++ app/src/main/java/com/bestdamn/fortyk/crusade/domain/Force.kt
@@ -1,17 +1,20 @@
 package com.bestdamn.fortyk.crusade.domain
 
+
 class Force(
     val name: String,
-    val faction: String,
-    val playerName: String,
-    val tally: Int,
-    val battlesWon: Int,
-    val requisitionPoints: Int,
-    val supplyLimit: Int,
-    val supplyUsed: Int,
-    val goalsInfoAndVictories: String,
+    val faction: String? = "",
+    val playerName: String? = "",
+    val tally: Int? = 0,
+    val battlesWon: Int? = 0,
+    val requisitionPoints: Int? = 0,
+    val supplyLimit: Int? = 0,
+    val supplyUsed: Int? = 0,
+    val goalsInfoAndVictories: String? = "",
 
     val units: MutableList<Unit>
 ) {
-
+    fun saveForce(force: Force) {
+        // TODO: write to shared prefs
+    }
 }
--- app/src/main/java/com/bestdamn/fortyk/crusade/domain/Unit.kt
+++ app/src/main/java/com/bestdamn/fortyk/crusade/domain/Unit.kt
@@ -3,43 +3,43 @@ package com.bestdamn.fortyk.crusade.domain
 class Unit (
     // card
     val name: String,
-    val role: String,
-    val faction: String,
-    val keywords: String,
-    val type: String,
-    val equipment: String,
-    val psychicPowers: String,
-    val warlordTraits: String,
-    val relics: String,
-    val upgradesAndAbilities: String,
+    val role: String? = "",
+    val faction: String? = "",
+    val keywords: String? = "",
+    val type: String? = "",
+    val equipment: String? = "",
+    val psychicPowers: String? = "",
+    val warlordTraits: String? = "",
+    val relics: String? = "",
+    val upgradesAndAbilities: String? = "",
 
-    val powerRating: Int,
-    val xp: Int,
-    val crusadePoints: Int,
+    val powerRating: Int? = 0,
+    val xp: Int? = 0,
+    val crusadePoints: Int? =0,
 
     // tallies
-    val battlesPlayed: Int,
-    val battlesSurvived: Int,
+    val battlesPlayed: Int? = 0,
+    val battlesSurvived: Int? = 0,
 
     // this battle
-    val unitsDestroyed: Int,
-    val unitsDestroyedPsychic: Int,
-    val unitsDestroyedRanged: Int,
-    val unitsDestroyedMelee: Int,
-    val agenda1: Int,
-    val agenda2: Int,
-    val agenda3: Int,
+    val unitsDestroyed: Int? = 0,
+    val unitsDestroyedPsychic: Int? = 0,
+    val unitsDestroyedRanged: Int? = 0,
+    val unitsDestroyedMelee: Int? = 0,
+    val agenda1: Int? = 0,
+    val agenda2: Int? = 0,
+    val agenda3: Int? = 0,
 
     // in total
-    val unitsDestroyedTotal: Int,
-    val unitsDestroyedPsychicTotal: Int,
-    val unitsDestroyedRangedTotal: Int,
-    val unitsDestroyedMeleeTotal: Int,
+    val unitsDestroyedTotal: Int? = 0,
+    val unitsDestroyedPsychicTotal: Int? = 0,
+    val unitsDestroyedRangedTotal: Int? = 0,
+    val unitsDestroyedMeleeTotal: Int? = 0,
 
     // rank
-    val currentRank: Int, // 1(blooded), 2(battle-hardened), 3(heroic) or 4(legendary)
-    val battleHonors: String,
-    val battleScars: String
+    val currentRank: Int? = 0, // 1(blooded), 2(battle-hardened), 3(heroic) or 4(legendary)
+    val battleHonors: String? = "",
+    val battleScars: String? = ""
 )
 {
 
