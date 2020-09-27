1095f7c75336352d375aa749b79971ffadc70329
Jordan <jcstoffer2@gmail.com>
2020-09-27 14:37:09 -0700

    Bind unit domain to view

--- app/src/main/java/com/bestdamn/fortyk/crusade/domain/Unit.kt
+++ app/src/main/java/com/bestdamn/fortyk/crusade/domain/Unit.kt
@@ -5,44 +5,44 @@ import java.util.*
 class Unit (
     // card
     val id: String = UUID.randomUUID().toString(),
-    val name: String?,
-    val role: String? = "",
-    val faction: String? = "",
-    val keywords: String? = "",
-    val type: String? = "",
-    val equipment: String? = "",
-    val psychicPowers: String? = "",
-    val warlordTraits: String? = "",
-    val relics: String? = "",
-    val upgradesAndAbilities: String? = "",
+    var name: String?,
+    var role: String? = "",
+    var faction: String? = "",
+    var keywords: String? = "",
+    var type: String? = "",
+    var equipment: String? = "",
+    var psychicPowers: String? = "",
+    var warlordTraits: String? = "",
+    var relics: String? = "",
+    var upgradesAndAbilities: String? = "",
 
-    val powerRating: Int? = 0,
-    val xp: Int? = 0,
-    val crusadePoints: Int? =0,
+    var powerRating: Int? = 0,
+    var xp: Int? = 0,
+    var crusadePoints: Int? =0,
 
     // tallies
-    val battlesPlayed: Int? = 0,
-    val battlesSurvived: Int? = 0,
+    var battlesPlayed: Int? = 0,
+    var battlesSurvived: Int? = 0,
 
     // this battle
-    val unitsDestroyed: Int? = 0,
-    val unitsDestroyedPsychic: Int? = 0,
-    val unitsDestroyedRanged: Int? = 0,
-    val unitsDestroyedMelee: Int? = 0,
-    val agenda1: Int? = 0,
-    val agenda2: Int? = 0,
-    val agenda3: Int? = 0,
+    var unitsDestroyed: Int? = 0,
+    var unitsDestroyedPsychic: Int? = 0,
+    var unitsDestroyedRanged: Int? = 0,
+    var unitsDestroyedMelee: Int? = 0,
+    var agenda1: Int? = 0,
+    var agenda2: Int? = 0,
+    var agenda3: Int? = 0,
 
     // in total
-    val unitsDestroyedTotal: Int? = 0,
-    val unitsDestroyedPsychicTotal: Int? = 0,
-    val unitsDestroyedRangedTotal: Int? = 0,
-    val unitsDestroyedMeleeTotal: Int? = 0,
+    var unitsDestroyedTotal: Int? = 0,
+    var unitsDestroyedPsychicTotal: Int? = 0,
+    var unitsDestroyedRangedTotal: Int? = 0,
+    var unitsDestroyedMeleeTotal: Int? = 0,
 
     // rank
-    val currentRank: Int? = 0, // 1(blooded), 2(battle-hardened), 3(heroic) or 4(legendary)
-    val battleHonors: String? = "",
-    val battleScars: String? = ""
+    var currentRank: String? = "Blooded", // Blooded, Hardened, Heroic, Legendary
+    var battleHonors: String? = "",
+    var battleScars: String? = ""
 )
 {
 
